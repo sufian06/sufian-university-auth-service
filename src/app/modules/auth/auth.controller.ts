d18d7bd1dab2efe5343a288205aefb37ede2de46
Abu sufian <75737338+sufian06@users.noreply.github.com>
2024-01-24 09:10:22 +0600

    loginUser updated at auth.controller file

--- src/app/modules/auth/auth.controller.ts
+++ src/app/modules/auth/auth.controller.ts
@@ -9,7 +9,7 @@ import { AuthService } from './auth.service';
 const loginUser = catchAsync(async (req: Request, res: Response) => {
   const { ...loginData } = req.body;
   const result = await AuthService.loginUser(loginData);
-  const { refreshToken, ...others } = result;
+  const { refreshToken } = result;
 
   // set refresh token into cookie
   const cookieOptions = {
@@ -22,7 +22,7 @@ const loginUser = catchAsync(async (req: Request, res: Response) => {
     statusCode: httpStatus.OK,
     success: true,
     message: 'User loggedin successfully!',
-    data: others,
+    data: result,
   });
 });
 
